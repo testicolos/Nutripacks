d24d791a53f33ca9fbe7747d2e23c27b5978eef4
Hussein <bitarhussein9@gmail.com>
2026-09-14 19:42:21 +0300

    chore: remove dormant payment status endpoint from testing build

--- app/api/payment/qiib/status/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getQiibGatewayStatus } from '../../../../../lib/qiib-payment';
-
-export async function GET() {
-  return NextResponse.json(getQiibGatewayStatus());
-}
